70aafef42c270aab32ddfa74d0dc6e9918463693
jun2-jun <hwanhwan402@gmail.com>
2025-04-16 16:03:36 +0900

    이메일전송까지 수정

--- project/app.py
+++ project/app.py
@@ -5,13 +5,18 @@ from datetime import datetime
 from routes.reserve import reserve_bp
 from routes.api import api_bp
 from flask_mail import Mail, Message
+from routes.submit_reservation import submit_bp
 
 app = Flask(__name__)
+app.secret_key = 'yougayoung123'
 init_db(app)
 
 app.register_blueprint(auth_bp, url_prefix="/auth")
 app.register_blueprint(reserve_bp, url_prefix="/api")
 app.register_blueprint(api_bp)
+app.register_blueprint(submit_bp)
+
+
 
 @app.route('/')
 def home():
@@ -55,17 +60,28 @@ def submit_reservation():
     message = request.form.get('message')
     email = request.form.get('email')
 
+    # 예약 완료 후 예약 정보 페이지 보여줌 (이메일 전송은 아님)
+    return render_template('submit_reservation.html',
+                           hospital=hospital,
+                           address=address,
+                           name=name,
+                           phone=phone,
+                           message=message,
+                           email=email)
+
+mail = Mail(app)
+
+# Flask-Mail 설정
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'  # 예시로 Gmail SMTP 서버 사용
-app.config['MAIL_PORT'] = 465
+app.config['MAIL_PORT'] = 587
 app.config['MAIL_USERNAME'] = 'doctorfutures2.0.0@gmail.com'  # 보내는 이메일 주소
-app.config['MAIL_PASSWORD'] = 'ohbu uulg lugu yxyl'  # 앱 비밀번호(2차인증)
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_PASSWORD'] = 'regd fery zlyu tbyg'  # 앱 비밀번호(2차인증)
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_DEFAULT_SENDER'] = 'doctorfutures2.0.0@gmail.com'  # 기본 보낸 사람
 
 
-
-app.route("/send_email", methods=["POST"])
+@app.route("/send_email", methods=["POST"])
 def send_email():
     hospital = request.form["hospital"]
     address = request.form["address"]
@@ -94,8 +110,14 @@ def send_email():
         msg = Message(subject=subject, recipients=[email])
         msg.body = body
 
+        try:
+            mail.send(msg)
+            print("이메일 전송 완료")
+        except Exception as e:
+            print("이메일 전송 실패:", e)
+
     else:
-        print('불가능')
+        print('불가능: 이메일 주소 없음.')
 
     return render_template("find.html")
 
