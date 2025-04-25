3b58855af6c100224f80d4349fa41fceb40d228d
Jh <167078745+eveses@users.noreply.github.com>
2025-04-25 13:22:06 +0900

    Update checkLogin.js

--- static/js/checkLogin.js
+++ static/js/checkLogin.js
@@ -6,8 +6,8 @@ document.addEventListener('DOMContentLoaded', () => {
     })
     .then(res => {
       if (res.status === 401) {
-        alert("로그인해야 됩니다.");
-        window.location.href = "/index";
+        alert("로그인이 필요한 서비스입니다.");
+        window.location.href = "/login";
       }
     })
     .catch(err => {
