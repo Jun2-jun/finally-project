108c7f8df4f06d2f691cb9f57ac7785873bf9af4
Jh <167078745+eveses@users.noreply.github.com>
2025-04-25 14:50:51 +0900

    Update checkLogin.js

--- static/js/checkLogin.js
+++ static/js/checkLogin.js
@@ -1,6 +1,6 @@
 // checkLogin.js
 document.addEventListener('DOMContentLoaded', () => {
-    fetch("http://192.168.219.189:5002/api/users/check-login", {
+    fetch("http://192.168.219.189:5002/api/check-login", {
       method: "GET",
       credentials: "include"
     })
