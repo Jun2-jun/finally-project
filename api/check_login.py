6433f85722cae546e032f74f9a9834ec3b182331
Jh <167078745+eveses@users.noreply.github.com>
2025-04-26 09:50:32 +0900

    주석추가

--- api/check_login.py
+++ api/check_login.py
@@ -4,6 +4,7 @@ from utils.auth import login_required  # 기존 데코레이터 import
 # /api/users prefix로 분리된 Blueprint
 check_login_bp = Blueprint('check_login', __name__, url_prefix='/api/users')
 
+#로그인 체크용 엔드포인트
 @check_login_bp.route('/check-login', methods=['GET'])
 @login_required
 def check_login_status():
